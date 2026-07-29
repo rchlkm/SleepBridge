3e7e223aaeb96e16b1995758ecba796b11f8cb8b
Rachel Kim <rachelkim.tech@gmail.com>
2026-07-29 16:45:57 -0700

    Initial Commit

---
+++ SleepBridge/SleepBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepBridgeApp.swift
+//  SleepBridge
+//
+//  Created by Rachel Kim on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SleepBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
